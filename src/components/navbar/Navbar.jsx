3a22b947cbe4c0da184408fbba0497087ef95903
Anas Khan <akhan2@ischoolconnect.com>
2024-06-17 10:59:51 +0530

    🎨 Navbar implemented

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,59 @@
 import React from "react";
-
+import styles from "./Navbar.module.css";
 const Navbar = () => {
-  return <nav className="">Navbar</nav>;
+  const links = [
+    {
+      href: "https://webflow.partnerlinks.io/column-image-scroll-animation",
+      text: "Clone this site",
+    },
+    {
+      href: "https://youtu.be/6rFm85ROcJg",
+      text: "Tutorial",
+    },
+    {
+      href: "https://jonasarleth.com/projekte",
+      text: "My Portfolio",
+    },
+  ];
+
+  const moreTutorialsLink = {
+    href: "https://formburg.com",
+    text: "More tutorials",
+  };
+  return (
+    <nav className={styles.nav_content}>
+      <div className={styles.nav_outer}>
+        <div className={styles.nav_inner}>
+          <div className={styles.nav_left}>
+            {links.map((link, index) => (
+              <div key={index} className="link-wrapper">
+                <a
+                  href={link.href}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className={styles.cta_link}
+                >
+                  {link.text}
+                </a>
+              </div>
+            ))}
+          </div>
+          <div className={styles.nav_right}>
+            <div className="link-wrapper">
+              <a
+                href={moreTutorialsLink.href}
+                target="_blank"
+                rel="noopener noreferrer"
+                className={styles.more_link_text}
+              >
+                {moreTutorialsLink.text}
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
