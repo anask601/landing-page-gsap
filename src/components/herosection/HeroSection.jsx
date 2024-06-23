0d8b1be2d33bd2ca90363529c1d14c398ac53d3a
Anas Khan <akhan2@ischoolconnect.com>
2024-06-23 17:25:20 +0530

    ✅ Update hero section component

--- src/components/herosection/HeroSection.jsx
+++ src/components/herosection/HeroSection.jsx
@@ -1,22 +1,22 @@
 import React from "react";
-import styles from "./HeroSection.module.css";
+import "./HeroSection.css";
 
 const HeroSection = () => {
   return (
-    <div className={styles.hero_section_container}>
-      <div className={styles.hero_section_text}>
-        <div className={styles.hero_subtitle}>Web design masterpiece</div>
-        <h1 className={styles.hero_title}>
+    <div className="hero_section_container">
+      <div className="hero_section_text">
+        <div className="hero_subtitle">Web design masterpiece</div>
+        <h1 className="hero_title">
           Create amazing 3-column scroll animations in Webflow
         </h1>
-        <p className={styles.hero_description}>
+        <p className="hero_description">
           Learn how to create a engaging three-column scroll animation with
           Webflow. Jonas Arleth guides you through inspiration, sticky elements,
           flexbox image positioning and fine-tuned animations. Inspired by:{" "}
           <a
             href="https://aim.obys.agency"
             target="_blank"
-            className={styles.hero_description_link}
+            className="hero_description_link"
           >
             https://aim.obys.agency
           </a>
